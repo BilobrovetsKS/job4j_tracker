f42534a930e7e39444883081d70519f37de1b9c6
Кирилл <Bilobrovets@mail.ru>
2021-02-09 23:34:48 +0700

    5.1. Зона видимости переменных [#417437]

---
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -0,0 +1,18 @@
+package ru.job4j.inheritance;
+
+public class JSONReport extends TextReport {
+
+    @Override
+    public String generate(String name, String body) {
+        return super.generate(name, body);
+    }
+
+    public static void main(String[] args) {
+        JSONReport report = new JSONReport();
+        String text = report.generate("name", "body");
+        System.out.println(text);
+    }
+}
+
+
+
