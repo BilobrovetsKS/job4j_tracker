325a2e949deade0a25c40c447b82cae99e84ff34
Кирилл <Bilobrovets@mail.ru>
2021-02-11 22:26:42 +0700

    3. Реализация профессий в коде [#417447]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -2,14 +2,9 @@ package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport {
 
-    @Override
-    public String generate(String name, String body) {
-        return super.generate(name, body);
-    }
-
     public static void main(String[] args) {
         JSONReport report = new JSONReport();
-        String text = report.generate("name:", "body:");
+        String text = report.generate("name", "body");
         System.out.println(text);
     }
 }
