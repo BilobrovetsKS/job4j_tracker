f70d5507c6a8ebbda842c81f2771ed7f4a24846b
Кирилл <Bilobrovets@mail.ru>
2021-02-23 22:27:20 +0700

    2. Модель данных. [#417449]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,12 @@
+package ru.job4j.pojo;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Иван Иванов Иванович");
+        student.setGroup("LB-221");
+        student.setDataAdmission("20.08.2020");
+
+        System.out.println("ФИО:" + student.getName() + System.lineSeparator() + "Группа:" + student.getGroup() +System.lineSeparator() + "Дата поступления:" + student.getDataAdmission());
+    }
+}
