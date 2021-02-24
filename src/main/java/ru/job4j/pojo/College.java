257d4f652c81923bd9a6356b3269b52654b555a6
Кирилл <Bilobrovets@mail.ru>
2021-02-24 20:24:59 +0700

    2. Модель данных. [#417449]

--- src/main/java/ru/job4j/pojo/College.java
+++ src/main/java/ru/job4j/pojo/College.java
@@ -7,6 +7,8 @@ public class College {
         student.setGroup("LB-221");
         student.setDataAdmission("20.08.2020");
 
-        System.out.println("ФИО:" + student.getName() + System.lineSeparator() + "Группа:" + student.getGroup() +System.lineSeparator() + "Дата поступления:" + student.getDataAdmission());
+        System.out.println("ФИО:" + student.getName() + System.lineSeparator() +
+                "Группа:" + student.getGroup() + System.lineSeparator() +
+                "Дата поступления:" + student.getDataAdmission());
     }
 }
