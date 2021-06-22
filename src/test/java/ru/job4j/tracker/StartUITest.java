4cd852536e1fa356de5294dc919bc3305c8eda7f
Кирилл <Bilobrovets@mail.ru>
2021-06-22 21:24:00 +0700

    9.1 Зависимость от System.out [#33568 #170320]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -10,23 +10,25 @@ public class StartUITest {
 
     @Test
     public void createItem() {
+        Output out = new StubOutput();
         Input in = new StubInput(new String[]{"0", "Item name", "1"});
         Tracker tracker = new Tracker();
-        UserAction[] actions = {new CreateAction(),
+        UserAction[] actions = {new CreateAction(out),
                 new ExitAction()};
-        new StartUI().init(in, tracker, actions);
+        new StartUI(out).init(in, tracker, actions);
         assertThat(tracker.findAll()[0].getName(), is("Item name"));
     }
 
     @Test
     public void replaceItem() {
+        Output out = new StubOutput();
     Tracker tracker = new Tracker();
     Item item = tracker.add(new Item("Replaced item"));
     String replacedName = "New Item";
     Input in = new StubInput(new String[] {"0", "1", replacedName, "1"});
-    UserAction[] actions = {new ReplaceAction(),
+    UserAction[] actions = {new ReplaceAction(out),
     new ExitAction()};
-    new StartUI().init(in, tracker, actions);
+    new StartUI(out).init(in, tracker, actions);
     assertThat(tracker.findById(item.getId()).getName(), is(replacedName));
     }
 
@@ -34,13 +36,48 @@ public class StartUITest {
 
     @Test
     public void deleteItem() {
+        Output out = new StubOutput();
         Tracker tracker = new Tracker();
         Item item = tracker.add(new Item("Deleted  Item"));
         Input in = new StubInput(new String[] {"0", "1", "1"});
-        UserAction[] actions = {new DeleteAction(),
+        UserAction[] actions = {new DeleteAction(out),
                 new ExitAction()};
-        new StartUI().init(in, tracker, actions);
+        new StartUI(out).init(in, tracker, actions);
         assertThat(tracker.findById(item.getId()), is(nullValue()));
+    }
+
+    @Test
+    public void findByIdItem() {
+        Output out = new StubOutput();
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("Find by Id Items"));
+        Input in = new StubInput(new String[] {"0", "1", "1"});
+        UserAction[] actions = {new FindByIdAction(out), new ExitAction()};
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(tracker.findById(item.getId()).getName(), is("Find by Id Items"));
+    }
+
+    @Test
+    public void findByNameItem() {
+        Output out = new StubOutput();
+        Tracker tracker = new Tracker();
+        String name = "Find by Name Items";
+        Item item = tracker.add(new Item(name));
+        Input in = new StubInput(new String[] {"0", name, "1"});
+        UserAction[] actions = {new FindByNameAction(out), new ExitAction()};
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(tracker.findById(item.getId()).getName(), is(name));
+    }
+
+    @Test
+    public void ShowAllItem() {
+        Output out = new StubOutput();
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("Show all Item"));
+        Input in = new StubInput(new String[] {"0", "Show all Item", "1"});
+        UserAction[] actions = { new ExitAction()};
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(tracker.findAll()[0].getName(), is("Show all Item"));
     }
 }
 
