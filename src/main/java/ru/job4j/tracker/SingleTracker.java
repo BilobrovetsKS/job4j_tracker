064bca4108913b0144821c5cca43bd9715d285c7
Кирилл <Bilobrovets@mail.ru>
2021-07-06 22:09:46 +0700

    2. Singleton [#94606]

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -1,6 +1,6 @@
 package ru.job4j.tracker;
 
-public final class SingleTracker {
+public class SingleTracker {
     private static SingleTracker instance;
     private Tracker tracker = new Tracker();
 
